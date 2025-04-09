035545f372c38f963c719ec031c040d69f5e4535
Eugene Kapombe <kapombek@gmail.com>
2025-04-09 14:49:03 +0300

    changed avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,8 @@
 export function Avatar({ src, alt }) {
 	return (
     <img
-      src="https://de.rc-cdn.community.thermomix.com/recipeimage/h2c0blh9-98115-736152-cfcd2-49vm52la/c2ff1fe4-4ea4-4f1b-84b9-db7cbcd0afb3/original/sprite-muffins.jpg"
-      alt='muffin-sprite'
+      src="https://de.rc-cdn.community.thermomix.com/recipeimage/h2c0blh9-98115-736152-cfcd2-49vm52la/45f9428a-4142-4ff1-9eb5-7b62f0d16a41/main/sprite-muffins.jpg"
+      alt="muffin-sprite"
       className="w-20 h-20 rounded-full border-2 border-blue-500"
     />
   );
